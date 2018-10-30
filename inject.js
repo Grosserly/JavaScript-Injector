b38a9b89a4b60b46467be1f5c291fb8782552d5c
Garlic OS® <14845347+Grosserly@users.noreply.github.com>
2018-10-30 17:43:40 -0500

    Button optimizations; comments
    All thanks to Kathack.

--- inject.js
+++ inject.js
@@ -1,52 +1,54 @@
+/**
+ *  JavaScript Injector
+ *  javascript:var%20d=document;var%20i=d.createElement('script');i.src='https://grosserly.github.io/js-inject/inject.js';d.body.appendChild(i);void(0);
+ */
 (function() {
 
     var modalWindow = document.createElement("dialog");
     modalWindow.setAttribute("open", "true");
     modalWindow.setAttribute("style", "position:absolute;padding:20px;border:0;box-shadow:0 0 50px;");
-    modalWindow.setAttribute("id", "javascript-injector-modal");
 
 
         var container = document.createElement("div");
         
             var closeButton = document.createElement("button");
             closeButton.appendChild(document.createTextNode("X"));
-            closeButton.setAttribute("onclick","closeBox()");
-            closeButton.setAttribute("style", "padding:2px 5px 2px;float:right");
+            closeButton.setAttribute("style", "position:absolute;top:0;right:0;padding:2px 5px 2px");
             
 
-            var header = document.createElement("div");
+            var header = document.createElement("a");
             header.appendChild(document.createTextNode("JavaScript Injector"));
+            header.setAttribute("style","font-size:16px");
+            header.setAttribute("href","https://github.com/Grosserly/js-inject/");
 
 
             var inputBox = document.createElement("textarea");
             inputBox.setAttribute("rows", "4");
             inputBox.setAttribute("cols", "50");
-            inputBox.setAttribute("style", "margin:15px 0 15px;");
-            inputBox.setAttribute("id", "javascript-injector-input-box");
+            inputBox.setAttribute("style", "margin:15px 0 15px");
             
             
             var submitButton = document.createElement("button");
             submitButton.appendChild(document.createTextNode("Inject"));
-            submitButton.setAttribute("onclick","injectCode()");
             submitButton.setAttribute("style", "float:left");
             
             
-            
+    // Append ALL the things!
     container.appendChild(closeButton);
     container.appendChild(header);
     container.appendChild(inputBox);
     container.appendChild(submitButton);
     modalWindow.appendChild(container);
     document.body.appendChild(modalWindow);
+    
+    // Delete injector box from page when X button clicked
+    closeButton.addEventListener('click', function () {
+        modalWindow.parentNode.removeChild(modalWindow);
+    }, true);
+    
+    // Run the JavaScript in the box when Inject button clicked
+    submitButton.addEventListener('click', function () {
+        eval(inputBox.value);
+    }, true);
 
 })();
-
-
-function injectCode() {
-    eval(document.getElementById("javascript-injector-input-box").value);
-}
-
-function closeBox() {
-    var modalWindow = document.getElementById("javascript-injector-modal");
-    modalWindow.parentNode.removeChild(modalWindow);
-}
