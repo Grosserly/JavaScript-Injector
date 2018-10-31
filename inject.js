d5798252ff2bdd68177481aff7708b9eb1d08454
Garlic OS® <14845347+Grosserly@users.noreply.github.com>
2018-10-31 09:31:27 -0500

    Update inject.js

--- inject.js
+++ inject.js
@@ -28,8 +28,8 @@
 	// Run the JavaScript in the box when Inject button clicked
 	modal.getElementsByTagName('button')[1].addEventListener('click', function() {
 		
-		try       	eval(modal.getElementsByTagName('textarea')[0].value);
-		catch(err)	alert(err.name + ":\n" + err.message);
+		try        { eval(modal.getElementsByTagName('textarea')[0].value); }
+		catch(err) { alert(err.name + ":\n" + err.message); }
 		
 	}, true);
 
