5c9ac59c9e13c730ea1d51c638859cfba4336214
Garlic OS® <14845347+Grosserly@users.noreply.github.com>
2018-10-31 09:03:23 -0500

    Remove unnecessary quotes; resize textarea; make textarea not wrap

--- inject.js
+++ inject.js
@@ -12,9 +12,9 @@
 	// Construct modal
 	modal.innerHTML = '<div>\
 <button style="position:absolute;top:0;right:0;padding:2px 5px 2px">X</button>\
-<a href="https://github.com/Grosserly/js-inject/" style="font-size:20px">JavaScript Injector</a>\
+<a href="https://github.com/Grosserly/js-inject/" style=font-size:20px>JavaScript Injector</a>\
 <div></div>\
-<textarea rows="12" cols="40" style="margin:15px 0 15px;font-family:monospace"></textarea>\
+<textarea rows=8 cols=60 style="margin:15px 0 15px;font-family:monospace;nowrap;overflow:auto"></textarea>\
 <div></div>\
 <button>Inject</button>\
 </div>';
