8daa8ff33a6d27ab1af920ef8c0db8cd0c345f9a
garlicOS® <14845347+Grosserly@users.noreply.github.com>
2018-12-12 08:27:28 -0600

    Only draggable in the header

--- inject.pretty.js
+++ inject.pretty.js
@@ -8,13 +8,13 @@
 
 	var modal = document.createElement("div");
 	document.body.appendChild(modal);
-	//modal.showModal(); // Required to make modal visible
 	modal.style = "position:absolute;top:40%;left:40%;padding:20px;border:0;box-shadow:0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);background-color:#F1F1F1"; // Add style to modal
 
 	// Construct modal
 	modal.innerHTML = '<div>\
+<div style=position:absolute;top:0;left:0;height:18%;width:100%></div>\
 <button style="position:absolute;top:0;right:0;padding:2px 5px 2px">X</button>\
-<a href="https://github.com/Grosserly/js-inject/" target=_blank style=font-size:20px>JavaScript Injector</a>\
+<a href="https://github.com/Grosserly/js-inject/" target=_blank style=position:absolute;font-size:20px>JavaScript Injector</a>\
 <br>\
 <textarea rows=6 cols=60 autocomplete=off autocorrect=off autocapitalize=off spellcheck=false style="margin:15px 0 15px;font-family:monospace;white-space:pre;overflow-wrap:normal;overflow:scroll;resize:both"></textarea>\
 <br>\
@@ -24,20 +24,21 @@
 
     
 	// Delete modal from page when X button clicked
-	modal.getElementsByTagName('button')[0].addEventListener('click', function() {
+	modal.getElementsByTagName("button")[0].addEventListener("click", function() {
 		modal.parentNode.removeChild(modal);
 	}, true);
 
 	// Run the JavaScript in the box when Inject button clicked
-	modal.getElementsByTagName('button')[1].addEventListener('click', function() {
+	modal.getElementsByTagName("button")[1].addEventListener("click", function() {
 		
-		try        { eval(modal.getElementsByTagName('textarea')[0].value); }
+		try        { eval(modal.getElementsByTagName("textarea")[0].value); }
 		catch(err) { alert(err.name + ":\n" + err.message); }
 		
 	}, true);
 
     var m1,m2,m3,m4 = 0;
-    modal.onmousedown = dragMouseDown;
+    var dragBar = modal.getElementsByTagName("div")[1];
+    dragBar.onmousedown = dragMouseDown;
 
 
     function dragMouseDown(e) {
